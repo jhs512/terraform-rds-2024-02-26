a39359e8e68f14df202dd3a5ad5941258314a5ce
jhs512 <jangka512@gmail.com>
2024-02-26 10:34:50 +0900

    work

---
+++ src/main/java/com/ll/terraformrds20240226/TerraformRds20240226Application.java
@@ -0,0 +1,13 @@
+package com.ll.terraformrds20240226;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerraformRds20240226Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TerraformRds20240226Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/terraformrds20240226/TerraformRds20240226ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.terraformrds20240226;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerraformRds20240226ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
